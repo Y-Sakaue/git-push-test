075015ac814b3ccd03effefc781179985d93bfb8
Yuji Sakaue <y.sakaue@cybermissions.co.jp>
2021-03-26 15:48:56 +0900

    deleted errors

--- Sample01.java
+++ Sample01.java
@@ -1,8 +1,8 @@
 public class Sample01{
 	public static void main(String[] args){
 		System.out.println("This is a Sample file.");
-		}
-	//this method is to say "Hello" when it is called.
-	public static void greetingRob(){ System.out.println("Hello.");}
-	
+
+
 	}
+	
+}
